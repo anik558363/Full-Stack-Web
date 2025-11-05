f8680b90766431dc54972687f2ce2b6344f47d5c
anik558363 <anik558363@gmail.com>
2025-11-05 09:53:44 +0600

    module-14 ay 2 number class done

---
+++ Module/M14C2/my-app/app/Http/Controllers/TestController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function sumOfTwoNumbers()
+    {
+        $a = 20;
+        $b = 30;
+        $c = $a + $b;
+
+        return $c;
+    }
+
+
+    public function responseTest(){
+
+        // return "test";
+        // return 222;
+        // return null;
+        // return [11, 5, 71, 2];
+
+        $userData = ["anik", "joy", "arpon"];
+        $total = 2020;
+
+
+      return response()->json([
+
+        "Batch" => "laravel-09",
+        "Teacher" => "sobuj",
+        'user' => $userData,
+        'total' => $total
+
+      ]);
+
+    }
+
+    public function redrectToExternalUrl(){
+
+        return redirect("https://chatgpt.com/c/690ac051-377c-8322-8a0d-cae6f37c8327");
+
+    }
+
+
+  public function redirectToInternalUrl(){
+
+    return redirect()->route('response.test');
+}
+
+
+public function returnFile(){
+
+    return response()->file(public_path('logo.jpg'));
+
+
+}
+
+
+
+
+}
---
+++ Module/M14C2/my-app/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\TestController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/response', function () {
+//     return "hello laravel";
+// });
+
+
+
+// Route::get('/response', function () {
+//     return 100;
+// });
+
+
+// Route::get('/response', function () {
+//     return true;
+// });
+
+
+
+Route::get('/response', function () {
+    return false;
+});
+
+
+
+Route::get('sumOfTwoNumbers', [TestController::class, 'sumOfTwoNumbers']);
+Route::get('responseTest', [TestController::class, 'responseTest'])->name('response.test');
+Route::get('redrectToExternalUrl', [TestController::class, 'redrectToExternalUrl']);
+Route::get('redirectToInternalUrl', [TestController::class, 'redirectToInternalUrl']);
+Route::get('returnFile', [TestController::class, 'returnFile']);
