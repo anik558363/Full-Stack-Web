b1ece4c17d0e43cae1f4068f28781333884d7266
Anik Mondol <anik(558363)@gmail.com>
2026-09-02 01:57:05 +0600

    feat: Enhance TestController with cookie and session management methods and update routes

--- Module/M14/M14C2/app/Http/Controllers/TestController.php
+++ Module/M14/M14C2/app/Http/Controllers/TestController.php
@@ -75,24 +75,84 @@ class TestController extends Controller
         return response()->file(public_path('coderixa_favicon.png'));
     }
 
-     public function pdfFileReturn()
+    public function pdfFileReturn()
     {
 
 
         return response()->file(public_path('img/anik(cv).pdf'));
     }
 
-  public function downloadFile()
-{
-    $file = public_path('img/anik(cv).pdf');
+    public function downloadFile()
+    {
+        $file = public_path('img/anik(cv).pdf');
 
-    if (!file_exists($file)) {
-        abort(404, 'CV file not found.');
+        if (!file_exists($file)) {
+            abort(404, 'CV file not found.');
+        }
+
+        return response()->download($file, 'anikmondal-cv.pdf', [
+            'Content-Type' => 'application/pdf',
+        ]);
     }
 
-    return response()->download($file, 'anikmondal-cv.pdf', [
-        'Content-Type' => 'application/pdf',
-    ]);
-}
+
+    public function setcookie()
+    {
+
+
+        return response('cookie has been set')->cookie('name', 'anik mondal', 60);
+    }
+
+
+    public function getcookie(Request $request)
+    {
+
+        $value = $request->cookie('name');
+
+        return response('Cookie value: ' . $value);
+    }
+
+
+    public function deleteCookie()
+    {
+        return response('Cookie deleted')->withoutCookie('name');
+
+    }
+
+
+    public function sessionSet(){
+
+
+    session(['name' => 'anik mondal', 'age' => 25]);
+
+    return response('Session has been set');
+
+    }
+
+
+    public function sessionGet(Request $request){
+
+        $name = $request->session()->get('name');
+        $age = $request->session()->get('age');
+
+        return response("Session values: Name - $name, Age - $age");
+    }
+
+    public function sessionDelete(Request $request){
+
+        $request->session()->forget('name');
+
+
+        return response('Session values deleted');
+    }
+
+
+    public function sessionFlush(Request $request){
+
+        $request->session()->flush();
+
+        return response('All session values deleted');
+    }
+
 
 }
--- Module/M14/M14C2/routes/web.php
+++ Module/M14/M14C2/routes/web.php
@@ -29,3 +29,18 @@ Route::get('/pdfFileReturn', [TestController::class, 'pdfFileReturn']);
 
 
 Route::get('/downloadFile', [TestController::class, 'downloadFile']);
+
+Route::get('/setcookie', [TestController::class, 'setcookie']);
+
+
+Route::get('/getcookie', [TestController::class, 'getcookie']);
+
+Route::get('/deletecookie', [TestController::class, 'deleteCookie']);
+
+Route::get('/sessionSet', [TestController::class, 'sessionSet']);
+
+Route::get('/sessionGet', [TestController::class, 'sessionGet']);
+Route::get('/sessionDelete', [TestController::class, 'sessionDelete']);
+
+Route::get('/sessionFlush', [TestController::class, 'sessionFlush']);
+
