24c93ef40a5e4718b25c6c8580c687bd3adb356d
Anik Mondol <anik(558363)@gmail.com>
2026-09-09 14:48:34 +0600

    Refactor navbar and update routes for improved navigation; enhance resume and projects pages with structured content and styling.

--- Module/M17/assignment-17/routes/web.php
+++ Module/M17/assignment-17/routes/web.php
@@ -4,16 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
+Route::get('/resume', function () {
+    return view('resume');
+})->name('resume');
 
-
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-
+Route::get('/projects', function () {
+    return view('projects');
+})->name('projects');
 
 Route::get('/contact', function () {
     return view('contact');
