02f2ea279100df2a0f89da6cf83ded255949c4b8
Anik Mondol <anik(558363)@gmail.com>
2026-09-11 02:59:47 +0600

    feat: Implement database migrations for profiles, products, orders, and order items; update LearnQueryController to return order data

--- Module/M18/pre-recorded-18/app/Http/Controllers/LearnQueryController.php
+++ Module/M18/pre-recorded-18/app/Http/Controllers/LearnQueryController.php
@@ -24,17 +24,51 @@ class LearnQueryController extends Controller
         // $users = DB::table('users')->select('id','name', 'email')->get();
 
 
-            // select method use like
+        // select method use like
         // $users = DB::table('users')->where('name', 'like', '%f%')->get();
 
 
-           // select method find, first
+        // select method find, first
         // $users = DB::table('users')->find(2);
-                $users = DB::table('users')->find(2);
+        // $users = DB::table('users')->find(2);
 
 
+        // inner join
+
+        // $users = DB::table('users')
+        //     ->join('roles', 'users.role_id', '=', 'roles.id')->select('users.name as user_name','users.email', 'roles.name as role_name')->get();
 
 
-        return $users;
+        // inner leftjoin
+
+        // $users = DB::table('roles')
+        //     ->leftJoin('users', 'users.role_id', '=', 'roles.id')->select('users.name as user_name','users.email', 'roles.name as role_name')->get();
+
+
+        // inner leftjoin
+
+        // $users = DB::table('users')
+        //     ->rightJoin('roles', 'users.role_id', '=', 'roles.id')->select('users.name as user_name', 'users.email', 'roles.name as role_name')->get();
+
+
+        // practical example with join
+
+        // $userProfile = DB::table('users')
+        //                ->join('profiles', 'profiles.user_id', '=', 'users.id')->get();
+
+
+        // $userProfile = DB::table('users')
+        //     ->join('profiles', 'profiles.user_id', '=', 'users.id')
+        //     ->where('users.id', 2)
+        //     ->first();
+
+
+        $orders = DB::table('order_items')
+                ->join('orders', 'orders.id', '=', 'order_items.order_id')
+                ->join('products', 'products.id', '=', 'order_items.product_id')
+                ->join('users', 'users.id', '=', 'orders.user_id')
+                ->get();
+
+        return $orders;
     }
 }
