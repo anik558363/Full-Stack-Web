9e5e05ea25c6c1380c6064412a77999aa10cfbdb
Anik Mondol <anik(558363)@gmail.com>
2026-09-13 16:13:26 +0600

    feat: Implement task management system with CRUD operations, status updates, and UI enhancements

--- Module/M18/task/routes/web.php
+++ Module/M18/task/routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\TaskController;
+
+Route::get('/', [TaskController::class, 'index'])
+    ->name('tasks.index');
+
+Route::post('/tasks', [TaskController::class, 'store'])
+    ->name('tasks.store');
+
+Route::put('/tasks/{task}', [TaskController::class, 'update'])
+    ->name('tasks.update');
+
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])
+    ->name('tasks.destroy');
+
+
+
+Route::put('/tasks/{task}/status', [TaskController::class, 'updateStatus'])
+    ->name('tasks.updateStatus');
