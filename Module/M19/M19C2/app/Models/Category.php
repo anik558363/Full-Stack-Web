bdc559b9dc2f2b91a821099d9732f8660c332af7
Anik Mondol <anik(558363)@gmail.com>
2026-09-19 12:05:39 +0600

    feat: Add relationships and fillable attributes in Category, InvventoryMovement, Order, OrderItem, Payment, and Product models

--- Module/M19/M19C2/app/Models/Category.php
+++ Module/M19/M19C2/app/Models/Category.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
 
-
+    use HasFactory;
 
     protected $fillable = ['name', 'parent_id', 'position'];
 
@@ -21,5 +22,10 @@ class Category extends Model
     {
 
         return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
     }
 }
