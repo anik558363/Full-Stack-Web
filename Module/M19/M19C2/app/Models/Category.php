c097252edfe2d73ebfbd24c10df9891aa85d47a6
Anik Mondol <anik(558363)@gmail.com>
2026-09-19 10:26:25 +0600

    feat: Enhance model structure by adding relationships and fillable attributes in Category, Customer, and CustomerProfile models

--- Module/M19/M19C2/app/Models/Category.php
+++ Module/M19/M19C2/app/Models/Category.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+
+
+
+    protected $fillable = ['name', 'parent_id', 'position'];
+
+    public function parent()
+    {
+
+        return $this->belongsTo(Category::class);
+    }
+
+    public function children()
+    {
+
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
